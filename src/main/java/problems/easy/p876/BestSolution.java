3e73f4d431b19135309be026eddd57a37f938f39
IlyaPIV <petermoloch@mail.ru>
2023-01-04 20:58:46 +0300

    p876

---
+++ src/main/java/problems/easy/p876/BestSolution.java
@@ -0,0 +1,12 @@
+package problems.easy.p876;
+
+public class BestSolution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
---
+++ src/main/java/problems/easy/p876/MySolution.java
@@ -0,0 +1,33 @@
+package problems.easy.p876;
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MySolution {
+    public ListNode middleNode(ListNode head) {
+
+        int currPos = 1;
+        int middlePos = 1;
+        ListNode middle = head;
+        ListNode curr = head;
+        while (curr.next != null) {
+            currPos++;
+            curr = curr.next;
+            if (middlePos * 2 <= currPos) {
+                middlePos++;
+                middle = middle.next;
+            }
+        }
+
+        return middle;
+    }
+}
