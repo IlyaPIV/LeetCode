228a7a017b153b6344ebaffa48d13db349ed8c1e
Ilya Piatrushyn <ilya.piatrushyn@sollers.eu>
2023-10-30 11:53:18 +0100

    p1137 - Python Solutions

---
+++ src/main/java/problems/easy/p1137/PythonSolution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        arr = [0] * (n + 1)
+        if n == 0 or n == 1:
+            return n
+        if n == 2:
+            return 1
+        arr[0] = 0
+        arr[1] = 1
+        arr[2] = 1
+        for i in range(3, n + 1):
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+        return arr[n]
